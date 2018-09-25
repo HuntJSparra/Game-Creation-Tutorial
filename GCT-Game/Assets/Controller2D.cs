3cfb0012f0d64ca6a30274f65a06000dda234fc3
JordanDeagan <jdeags@gmail.com>
2018-09-25 18:19:25 -0400

    Added Jordan's controller

---
+++ GCT-Game/Assets/Controller2D.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller2D : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private float velMod;
+    private float jumpForce;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        velMod = 5;
+        jumpForce = 300;
+	}
+
+    private void Update()
+    {
+        Vector2 jump = new Vector2(0.0f, jumpForce);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(jump);
+        }
+        
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal")*velMod, rb.velocity.y);
+    }
+}
