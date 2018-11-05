79bfa3f42349fbdb0decce77d7e347563cefe446
Hunt Sparra <huntehlaboratories@gmail.com>
2018-11-05 12:43:52 -0500

    Hooked up running animation in Hunt scene

---
+++ GCT-Game/Assets/CloneScriptAnimated.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//add code for turning off box colider
+
+
+
+//William
+public class CloneScriptAnimated : MonoBehaviour {
+
+    Dictionary<int, Vector3[]> rewindDict = new Dictionary<int, Vector3[]>();
+    public GameObject player;
+    private int currentTime;
+    private Rigidbody2D rb; // Rigidbody for this object
+    private BoxCollider2D bc; // box collider 
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        currentTime = player.GetComponent<Controller2DAnimated>().getTime();
+        if (Input.GetKey("e")) // hold down e to rewind
+        {
+            // setting position if this clone is in the dictionary
+            if (rewindDict.ContainsKey(currentTime)) 
+            {
+                goingBackwards();
+            }
+            else // while rewinding and this clone is not supposed to be displayed right now
+            {
+                GetComponent<SpriteRenderer>().enabled = false;
+                bc.enabled = false;
+            }
+        }
+        else // going forward
+        {
+            if(rewindDict.ContainsKey(currentTime)) 
+            {
+                Vector3 rewindingPos = goingForwards();
+
+                // paradox checking by velocity
+                if (!transform.position.Equals(rewindingPos))
+                {
+                    print("Paradox");
+                }
+            }
+            else // this clone isn't in the game at this time so disappear 
+            {
+                GetComponent<SpriteRenderer>().enabled = false;
+                bc.enabled = false;
+            }
+        }
+    }
+    
+    public void updateDictionary(Dictionary<int, Vector3[]> temp)
+    {
+        rewindDict = temp;
+        print("Got a new dictionary");
+        currentTime = player.GetComponent<Controller2DAnimated>().getTime();
+    }
+
+    public void goingBackwards()
+    {
+        Vector3 rewindingPos = rewindDict[currentTime][0];
+        Vector2 rewindingVelo = rewindDict[currentTime][1];
+        //print("Rewinding to this place " + rewindingPos);
+        GetComponent<SpriteRenderer>().enabled = true;
+        transform.position = rewindingPos;
+    }
+
+    public Vector3 goingForwards()
+    {
+        GetComponent<SpriteRenderer>().enabled = true;
+        Vector3 rewindingPos = rewindDict[currentTime][0];
+        Vector2 rewindingVelo = rewindDict[currentTime][1];
+
+        //Quaternion empty = new Quaternion();
+        //transform.SetPositionAndRotation(rewindingPos, empty);
+        // turns the bounding box back on 
+        bc.enabled = true;
+        rb.velocity = rewindingVelo;
+        return rewindingPos;
+    }
+}
---
+++ GCT-Game/Assets/Controller2DAnimated.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//William
+public class Controller2DAnimated : MonoBehaviour {
+
+    private Rigidbody2D rb; // Rigidbody for this object
+    private const float velMod = 5; // how fast we scale movement left and right
+    private const float jumpForce = 300; // how much of a force we're applying upwards when we jump
+    private Dictionary<int, Vector3[]> rewindDict = new Dictionary<int, Vector3[]>(); // The array at [0] is position and at [1] is velocity
+    private bool rewindingStartedLastFrame = true; // used for instantiating clone
+    private int currentTime; // current time that we are on (is subtracted while we're going back in time)
+    public GameObject guy; // the starter clone
+    private int spawnFrame = 1; // how far back we copy the dictionary too
+
+    // Animation-Related
+    private Animator animator;
+    private SpriteRenderer sr;
+
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+    
+    private void Update()
+    {
+        if (Input.GetKey("e")) // if rewinding
+        {
+            // setting up a clone
+            if(rewindingStartedLastFrame)
+                createClone();
+
+            // Sets the current time back and sets spawn frame to the right value;
+            if (currentTime > 1)
+            {
+                currentTime = currentTime - 1;
+                spawnFrame = currentTime;
+            }
+        }
+        else // playable
+        {
+            // The current time 
+            currentTime = currentTime + 1;
+
+            // adds current location to rewinding dictionary
+            rewindDict[currentTime] = new Vector3[] { transform.position, rb.velocity };
+
+            jump();
+
+            // turns the players movement back on if they just stopped rewinding, in use so the character stops while rewinding
+            if(rewindingStartedLastFrame == false)
+            {
+                rb.bodyType = RigidbodyType2D.Dynamic;
+            }
+
+            // used for initializing clones
+            rewindingStartedLastFrame = true;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (Input.GetKey("e")) // hold down e to rewind
+        {
+            rb.bodyType = RigidbodyType2D.Static;
+
+            // Animation-related
+            animator.SetBool("Running", false);
+        }
+        else
+        {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+            rb.velocity = new Vector2(Input.GetAxis("Horizontal") * velMod, rb.velocity.y);
+
+            // Animation-related
+            if (Mathf.Abs(Input.GetAxis("Horizontal")) > 0.3)
+                animator.SetBool("Running", true);
+            else
+                animator.SetBool("Running", false);
+
+            // Face the correct direction
+            if (Input.GetAxis("Horizontal") > 0)
+                sr.flipX = false;
+            else if (Input.GetAxis("Horizontal") < 0)
+                sr.flipX = true;
+        }
+    }
+
+    public int getTime()
+    {
+        return currentTime;
+    }
+
+    public void createClone()
+    {
+        Quaternion empty = new Quaternion();
+        Vector3 currentPos = transform.position;
+        GameObject clone = Instantiate(guy, currentPos, empty);
+
+        clone.GetComponent<SpriteRenderer>().enabled = true;
+        // for deep copying
+        Dictionary<int, Vector3[]> temp = new Dictionary<int, Vector3[]>();
+        for (int i = spawnFrame; i < currentTime; i++)
+        {
+            temp.Add(i, rewindDict[i]);
+        }
+        clone.GetComponent<CloneScript>().updateDictionary(temp);
+
+        rewindingStartedLastFrame = false;
+    }
+
+    public void jump()
+    {
+        Vector2 jump = new Vector2(0.0f, jumpForce);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(jump);
+        }
+    }
+}
