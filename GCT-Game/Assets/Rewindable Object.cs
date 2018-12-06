6ff8399611c567b7bbe384871c1ea21bb04c2b4c
William Pembleton <william.pembleton17@ncf.edu>
2018-12-06 13:41:29 -0500

    Made rewindable object interface

---
+++ GCT-Game/Assets/Rewindable Object.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewindableObject : MonoBehaviour {
+
+    private Dictionary<int, Vector3> previousLocations = new Dictionary<int, Vector3>(); // The array at [0] is position
+    private int currentTime; // current time that we are on (is subtracted while we're going back in time)
+    public GameObject player;
+
+    //Sound FX related
+    public AudioSource soundFXSource;
+    //how to make new sounds
+    public AudioClip ExampleSound;
+
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey("e")) // if rewinding
+        {
+            goBackwards();
+        }
+        else // playable
+        {
+            writeDownLocation();
+            //how to play sounds
+            /*
+            if(someCondition)
+            {
+                //sets the clip for the sound object
+                soundFXSource.clip = FakeSound;
+                //plays the clip in the clip variable
+                soundFXSource.Play();
+            }
+            */
+        }
+    }
+
+    private void goBackwards()
+    {
+        Vector3 rewindingPos = previousLocations[currentTime];
+        transform.position = rewindingPos;
+    }
+
+    private void writeDownLocation()
+    {
+        currentTime = player.GetComponent<Controller2DAnimated>().getTime();
+        //Write down location
+        previousLocations[currentTime] = transform.position;
+    }
+}
